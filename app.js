9aa40880880f54cf6c6b3b7f530bc3ac81dd4696
Luke Bandy <luke.bandy@gmail.com>
2018-04-02 15:00:23 -0400

    removed hidden class

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// your code here!
+$(document).ready(() => {
+  $("button").on("click", () => {
+    $("dl").show(".hidden");
+  })
+  counter = function() {
+  	var value = $('#user-text').val();
+    var regex = /\s+/gi;
+    var wordCount = value.trim().replace(regex, ' ').split(' ').length;
+    console.log(wordCount); 
+  }
+});
